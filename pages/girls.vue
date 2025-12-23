261a96340b1120e7086fa83b57dc85dc020b1392
Jessica Voytek <jessica.voytek@gmail.com>
2025-12-23 12:12:13 -0800

    Move toggle into toolbar

--- pages/girls.vue
+++ pages/girls.vue
@@ -1,40 +1,5 @@
-<script setup lang="ts">
-  const seasonsStore = useSeasonsStore();
-  const notificationHelpers = useNotificationHelpers();
-
-  const publishGirlRequestForm = computed({
-    get: () => seasonsStore.currentSeason?.publish_girl_request_form ?? false,
-    set: async (value) => {
-      if (seasonsStore.currentSeason?.id && seasonsStore.currentSeason.id > 0) {
-        try {
-          await seasonsStore.upsertSeason({
-            ...seasonsStore.currentSeason,
-            publish_girl_request_form: value,
-          });
-          await seasonsStore.fetchSeasons();
-        } catch (error) {
-          notificationHelpers.addError(error as Error);
-        }
-      }
-    },
-  });
-</script>
-
 <template>
   <div class="grid grid-cols-12 gap-8">
-    <div class="col-span-12">
-      <div class="card">
-        <div class="flex items-center gap-4 mb-4">
-          <ToggleSwitch
-            v-model="publishGirlRequestForm"
-            input-id="publish-girl-request-form"
-          />
-          <label for="publish-girl-request-form" class="font-bold"
-            >Publish Girl Request Form</label
-          >
-        </div>
-      </div>
-    </div>
     <GirlList />
   </div>
 </template>
