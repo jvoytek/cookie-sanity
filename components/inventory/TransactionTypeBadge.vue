8b329fb4dba544f0bc52c2d580258691e16234ee
Jessica Voytek <jessica.voytek@gmail.com>
2026-09-01 08:58:33 -0700

    Girl Transactions mobile friendly; loads SLOW;

---
+++ components/inventory/TransactionTypeBadge.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+  const props = defineProps<{
+    type: string;
+  }>();
+  const transactionsStore = useTransactionsStore();
+  const transactionHelpers = useTransactionHelpers();
+
+  const getTransactionTypeIconClass = (type: string) => {
+    switch (type) {
+      case 'T2G':
+        return 'pi pi-arrow-down mr-1';
+      case 'T2G(B)':
+        return 'pi pi-arrow-down mr-1';
+      case 'T2G(VB)':
+        return 'pi pi-arrow-down mr-1';
+      case 'G2T':
+        return 'pi pi-arrow-up mr-1';
+      case 'G2G':
+        return 'pi pi-arrow-right-arrow-left mr-1';
+      case 'DIRECT_SHIP':
+        return 'pi pi-truck mr-1';
+      default:
+        return '';
+    }
+  };
+</script>
+<template>
+  <Badge :severity="transactionHelpers.transactionTypeBadgeSeverity(props.type)"
+    ><i :class="getTransactionTypeIconClass(props.type)"></i
+    >{{ transactionsStore.friendlyTransactionTypes(props.type) }}</Badge
+  >
+</template>
